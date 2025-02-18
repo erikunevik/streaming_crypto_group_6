b476bcbe529d1790fae7e17627e7db9dd7313d27
John Sandsjö <john.sandsjo@gmail.com>
2025-02-18 15:46:26 +0100

    adding better currency handling

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+EXCHANGERATEAPI_KEY = os.getenv("CURRENCY_KEY")
+
 COINMARKET_API = os.getenv("API_KEY")
+
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DBNAME = os.getenv("POSTGRES_DB")
--- src/currencies.py
+++ src/currencies.py
@@ -1,3 +1,20 @@
+from requests import Session
+from constants import EXCHANGERATEAPI_KEY
+import json
+
+def get_latest_exchange_rate():
+    currency_url = "https://api.exchangeratesapi.io/v1/latest"
+    parameters = {
+        "access_key": EXCHANGERATEAPI_KEY,
+        "base" : "EUR",
+        "symbols" : "SEK,DKK,NOK",
+    }
+    session = Session()
+
+    response = session.get(currency_url, params=parameters)
+
+    return json.loads(response.text)["rates"]
+
 currencies_dict = {
     "SEK": 10.72,
     "DKK": 7.34,
