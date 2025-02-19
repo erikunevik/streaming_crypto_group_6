d3a964b9b45ee62ed8478b66e163511d4128f321
John Sandsjö <john.sandsjo@gmail.com>
2025-02-19 10:08:10 +0100

    currency fix

--- src/currencies.py
+++ src/currencies.py
@@ -1,23 +1,24 @@
-from requests import Session
-from constants import EXCHANGERATEAPI_KEY
-import json
+currencies_dict = {
+    "SEK": 10.72,
+    "DKK": 7.34,
+    "NOK" : 11.74,
+    "EUR" : 0.96
+}
 
-def get_latest_exchange_rate():
-    currency_url = "https://api.exchangeratesapi.io/v1/latest"
-    parameters = {
-        "access_key": EXCHANGERATEAPI_KEY,
-        "base" : "EUR",
-        "symbols" : "SEK,DKK,NOK",
-    }
-    session = Session()
+#--------ran out of api usage on free plan
+# from requests import Session
+# from constants import EXCHANGERATEAPI_KEY
+# import json
 
-    response = session.get(currency_url, params=parameters)
+# def get_latest_exchange_rate():
+#     currency_url = "https://api.exchangeratesapi.io/v1/latest"
+#     parameters = {
+#         "access_key": EXCHANGERATEAPI_KEY,
+#         "base" : "EUR",
+#         "symbols" : "SEK,DKK,NOK",
+#     }
+#     session = Session()
 
-    return json.loads(response.text)["rates"]
+#     response = session.get(currency_url, params=parameters)
 
-# currencies_dict = {
-#     "SEK": 10.72,
-#     "DKK": 7.34,
-#     "NOK" : 11.74,
-#     "EUR" : 0.96
-# }
+#     return json.loads(response.text)["rates"]
